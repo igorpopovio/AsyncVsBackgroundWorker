6a978ec7e30e166b3585b8cf56e74a3e312d4291
Igor Popov <igor@igorpopov.io>
2018-06-17 08:55:41 +0300

    Implement async task with progress report
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- AsyncVsBackgroundWorker/MainWindow.xaml.cs
+++ AsyncVsBackgroundWorker/MainWindow.xaml.cs
@@ -1,10 +1,36 @@
-﻿namespace AsyncVsBackgroundWorker
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncVsBackgroundWorker
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void AsyncTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            IProgress<int> progress = new Progress<int>(percentCompleted =>
+            {
+                AsyncTaskProgressBar.Value = percentCompleted;
+            });
+
+            AsyncTaskButton.IsEnabled = false;
+            await Task.Run(() =>
+            {
+                progress.Report(0);
+                foreach (var i in Enumerable.Range(1, 4))
+                {
+                    Thread.Sleep(1000);
+                    progress.Report(i * 25);
+                }
+            });
+            AsyncTaskButton.IsEnabled = true;
         }
     }
 }
